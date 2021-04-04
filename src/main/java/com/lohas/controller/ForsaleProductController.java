ed61d249ebc85489cfa139be6a08b1237bdd1c73
Mrcopytuo <920411694@qq.com>
2021-04-05 00:44:17 +0800

    商品模块完成增删改查

--- src/main/java/com/lohas/controller/ForsaleProductController.java
+++ src/main/java/com/lohas/controller/ForsaleProductController.java
@@ -1,11 +1,55 @@
 package com.lohas.controller;
 
+import com.lohas.request.CreateForsaleProductRequest;
+import com.lohas.request.DeleteForsaleProductRequest;
+import com.lohas.request.QueryAnnouncementByShopRequest;
+import com.lohas.request.UpdateForsaleProductRequest;
+import com.lohas.service.ForsaleProductService;
+import com.lohas.view.ForsaleProductPage;
+import com.lohas.view.Status;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Api(tags = "折扣商品相关模块")
 @Controller
 @RequestMapping(path="/api/forsaleproduct/")
 public class ForsaleProductController {
+    @Autowired
+    ForsaleProductService forsaleProductService;
+
+    @ApiOperation(value = "发布折扣商品")
+    @PostMapping(path="/create")
+    @ResponseBody
+    public Status createForsaleProduct(@RequestBody CreateForsaleProductRequest forsaleProductRequest, HttpServletRequest request){
+        return forsaleProductService.createForsaleProduct(forsaleProductRequest, request);
+    }
+
+    @ApiOperation(value = "修改折扣商品")
+    @PostMapping(path="/update")
+    @ResponseBody
+    public Status updateForsaleProduct(@RequestBody UpdateForsaleProductRequest updateForsaleProductRequest, HttpServletRequest request){
+        return forsaleProductService.updateForsaleProduct(updateForsaleProductRequest, request);
+    }
+
+    @ApiOperation(value = "删除折扣商品")
+    @PostMapping(path="/delete")
+    @ResponseBody
+    public Status deleteForsaleProduct(@RequestBody DeleteForsaleProductRequest deleteForsaleProductRequest, HttpServletRequest request){
+        return forsaleProductService.deleteForsaleProduct(deleteForsaleProductRequest, request);
+    }
+
+    @ApiOperation(value = "查询店家的折扣商品")
+    @PostMapping(path="/query")
+    @ResponseBody
+    public ForsaleProductPage getForsaleProductOfOneShop(@RequestBody QueryAnnouncementByShopRequest queryForsaleProductByShopRequest, HttpServletRequest request){
+        return forsaleProductService.getForsaleProductOfOneShop(queryForsaleProductByShopRequest, request);
+    }
 }
--- src/main/java/com/lohas/dao/ForsaleProductDAO.java
+++ src/main/java/com/lohas/dao/ForsaleProductDAO.java
@@ -1,7 +1,12 @@
 package com.lohas.dao;
 
 import com.lohas.model.ForsaleProduct;
+import com.lohas.model.Shop;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ForsaleProductDAO  extends CrudRepository<ForsaleProduct,Integer>{
+    ForsaleProduct findByProductId(Integer productId);
+    Page<ForsaleProduct> findAllByShop(Shop shop, Pageable pageable);
 }
